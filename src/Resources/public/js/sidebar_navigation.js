8ff630c9c7f47234ab4bf66dbaa41fffea1d8268
Marko Cupic <m.cupic@gmx.ch>
2021-06-15 19:58:48 +0200

    clean code and add aria-expanded attribute

--- src/Resources/public/js/sidebar_navigation.js
+++ src/Resources/public/js/sidebar_navigation.js
@@ -1,18 +1,61 @@
+/*
+ * This file is part of Contao Sidebar Navigation.
+ *
+ * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/contao-sidebar-navigatio
+ */
 jQuery(document).ready(function () {
-    var elToggler = jQuery('<button class="toggle-submenu" role="button"> </button>');
-    jQuery(elToggler).insertAfter(".sidebar-navigation li.submenu > a, .sidebar-navigation li.submenu > strong");
-    jQuery('.sidebar-navigation li.submenu.trail, .sidebar-navigation li.active').addClass('expanded');
+    // Insert dropdown toggle button
+    jQuery('<button class="toggle-submenu" role="button"></button>')
+    .insertAfter(".sidebar-navigation li.submenu > a, .sidebar-navigation li.submenu > strong");
 
+    // Add aria-expanded attribute and expanded class
+    jQuery('.sidebar-navigation li.submenu:not(.trail)')
+    .attr('aria-expanded', 'false');
+
+    jQuery('.sidebar-navigation li.submenu.trail, .sidebar-navigation li.active')
+    .addClass('expanded')
+    .attr('aria-expanded', 'true');
+
+    // Handle click events on the dropdown toggle button
     setTimeout(function () {
         jQuery('.sidebar-navigation .toggle-submenu').click(function (e) {
             e.preventDefault();
             e.stopPropagation();
 
-            jQuery(this).closest('li').find('li.expanded').removeClass('expanded').children('ul').slideUp();
-            // Close opened sibling
-            jQuery(this).closest('li').siblings('li.expanded').removeClass('expanded').children('ul').slideUp();
-            // open/close item
-            jQuery(this).closest('li').toggleClass('expanded').children('ul').slideToggle();
+            // Close menu
+            jQuery(this).closest('li:not(.expanded)')
+            .find('li.expanded')
+            .removeClass('expanded')
+            .attr('aria-expanded', 'false')
+            .children('ul')
+            .slideUp();
+
+            // Close opened siblings
+            jQuery(this).closest('li')
+            .siblings('li.expanded')
+            .removeClass('expanded')
+            .attr('aria-expanded', 'false')
+            .children('ul')
+            .slideUp();
+
+            // Open/close item
+            if (jQuery(this).closest('li').hasClass('expanded')) {
+                jQuery(this).closest('li')
+                .removeClass('expanded')
+                .attr('aria-expanded', 'false')
+                .children('ul')
+                .slideUp();
+            } else {
+                jQuery(this).closest('li')
+                .addClass('expanded')
+                .attr('aria-expanded', 'true')
+                .children('ul')
+                .slideDown();
+            }
         });
     }, 20);
 });
