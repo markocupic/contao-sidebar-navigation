a68a7601bd4593bb9ed091f768d0b821565a12b3
Marko Cupic <m.cupic@gmx.ch>
2024-11-09 22:18:19 +0100

    Remove date from phpdoc

--- .ecs/config/set/header_comment_fixer.php
+++ .ecs/config/set/header_comment_fixer.php
@@ -12,7 +12,7 @@ return static function (ECSConfig $ECSConfig): void {
     $services
         ->set(HeaderCommentFixer::class)
         ->call('configure', [[
-            'header' => "This file is part of Contao Sidebar Navigation.\n\n(c) Marko Cupic ".date("Y")." <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/contao-sidebar-navigation",
+            'header' => "This file is part of Contao Sidebar Navigation.\n\n(c) Marko Cupic <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/contao-sidebar-navigation",
         ]])
     ;
 };
